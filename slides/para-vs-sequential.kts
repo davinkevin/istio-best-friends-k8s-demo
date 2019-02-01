3fead3bf2c782fbdd858c7b89cdfb822c065363d
davinkevin <davin.kevin@gmail.com>
2019-02-01 19:07:45 +0100

    chore(slides): add snippets with kotlin code for slides to integrates into carbon.now.sh

---
+++ slides/para-vs-sequential.kts
@@ -0,0 +1,14 @@
+
+@Component
+class MainHandler(val foo: FooService, val bar: BarService) {
+
+    fun para(request: ServerRequest) = foo
+            .fetch()
+            .zipWith(bar.fetch())
+            .flatMap { ok().syncBody(HelloMessage("Touraine Tech 2019", "parallel")) }
+
+    fun seq(request: ServerRequest) = foo
+            .fetch()
+            .flatMap { bar.fetch() }
+            .flatMap { ok().syncBody(HelloMessage("Touraine Tech 2019", "sequential")) }
+}
